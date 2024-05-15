2e8b463cb968fb1de22d61f15cec28d9f8b4f449
aditya <adityadewhy@gmail.com>
2024-05-15 13:03:13 +0530

    make sigin changes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,19 +1,26 @@
 const express = require('express')
 import { User } from '../db';
 import {z} from Zod;
+import JWT_SECRET from '../config';
 
 const router = express.Router();
 
-const signUpSchema = z.object({
+const signupSchema = z.object({
     firstName: z.string(),
     lastName:z.string();
     username: z.string().min(5, 'Username must be at least 5 characters'),
     password: z.string().min(8, 'Password must contain at least 8 characters'),
 })
 
+const siginSchema = z.object({
+    username: z.string().min(5, "username doesnt meet min no of characters"),
+    password: z.string().min(8, "password doesnt meet min no of characters")
+    
+})
+
 router.post('/signup',async(req,res)=>{
     const body = req.body;
-    const {success} = signUpSchema.safeParse(req.body);
+    const {success} = signupSchema.safeParse(req.body);
     if(!success){
         res.json({
             message: "Email already taken / Incorrect inputs"
@@ -37,9 +44,23 @@ router.post('/signup',async(req,res)=>{
         password: body.password,
     })
 
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET);
+
+    res.status(200).json({
+        message:"user created successfully",
+        token: token,
+    })
+
 })
 
-router.post('/sigin',(req,res)=>{
+router.post('/signin',(req,res)=>{
+    const body = req.body;
+
+
 
 })
 

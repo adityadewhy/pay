04fd9a060aaa47090685121627b49eb16c59be4c
aditya <adityadewhy@gmail.com>
2024-05-15 16:57:33 +0530

    signup signin done, formatting left

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,4 @@
+// not sure about importing jsonwebtoken like this
+import 'jsonwebtoken'; 
 const JWT_SECRET = "walletPay"
 module.exports = JWT_SECRET;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -15,12 +15,12 @@ const signupSchema = z.object({
 const siginSchema = z.object({
     username: z.string().min(5, "username doesnt meet min no of characters"),
     password: z.string().min(8, "password doesnt meet min no of characters")
-    
+
 })
 
 router.post('/signup',async(req,res)=>{
     const body = req.body;
-    const {success} = signupSchema.safeParse(req.body);
+    const {success} = signupSchema.safeParse(body);
     if(!success){
         res.json({
             message: "Email already taken / Incorrect inputs"
@@ -57,10 +57,33 @@ router.post('/signup',async(req,res)=>{
 
 })
 
-router.post('/signin',(req,res)=>{
+router.post('/signin', async(req,res)=>{
     const body = req.body;
+    const {success} = signin.safeParse(body);
+    if(!success){
+        res.status(411).json({
+            message:"incorrect inputs",
+        })
+    }
+    const user = await User.findOne({
+        username: body.username,
+        password: body.password,
+    })
 
+    if(user){
+        const token = jwt.sign({
+            userId : user._id,
+        }, JWT_SECRET)
 
+        return res.status(200).json({
+            message:"username and password match found",
+            token: token,
+        })
+    }
+
+    res.status(411).json({
+        message:"Error while logging in",
+    })
 
 })
 

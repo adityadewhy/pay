4027c95671a9c4833b214c93cd00ba79994e2cff
aditya <adityadewhy@gmail.com>
2024-05-16 17:27:14 +0530

    authMiddleware done

---
+++ backend/middleware.js
@@ -0,0 +1,27 @@
+const {JWT_SECRET} = require("./config");
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+	const headerAuth = req.headers.authorization;
+
+	if (!headerAuth.startsWith("Bearer ")) {
+		return res.status(403).json({
+			message: "token missing",
+		});
+	}
+
+	const token = headerAuth.split(" ")[1];
+
+	try {
+		const decoded = jwt.verify(token, JWT_SECRET);
+		req.userId = decoded.userId;
+
+		next();
+	} catch (error) {
+		return res.status(403).json({
+			message: "error 403 forbidden error",
+		});
+	}
+};
+
+module.exports = authMiddleware;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,90 +1,93 @@
-const express = require('express')
-import { User } from '../db';
-import {z} from Zod;
-import JWT_SECRET from '../config';
+const express = require("express");
+import {User} from "../db";
+const z = require("zod");
+import JWT_SECRET from "../config";
 
 const router = express.Router();
 
 const signupSchema = z.object({
-    firstName: z.string(),
-    lastName:z.string();
-    username: z.string().min(5, 'Username must be at least 5 characters'),
-    password: z.string().min(8, 'Password must contain at least 8 characters'),
-})
+	firstName: z.string(),
+	lastName: z.string(),
+	username: z.string().min(5, "Username must be at least 5 characters"),
+	password: z.string().min(8, "Password must contain at least 8 characters"),
+});
 
 const siginSchema = z.object({
-    username: z.string().min(5, "username doesnt meet min no of characters"),
-    password: z.string().min(8, "password doesnt meet min no of characters")
+	username: z.string().min(5, "username doesnt meet min no of characters"),
+	password: z.string().min(8, "password doesnt meet min no of characters"),
+});
 
-})
+router.post("/signup", async (req, res) => {
+	const body = req.body;
+	const {success} = signupSchema.safeParse(body);
+	if (!success) {
+		res.json({
+			message: "Email already taken / Incorrect inputs",
+		});
+	}
 
-router.post('/signup',async(req,res)=>{
-    const body = req.body;
-    const {success} = signupSchema.safeParse(body);
-    if(!success){
-        res.json({
-            message: "Email already taken / Incorrect inputs"
-        })
-    }
+	const checkExistingUser = await User.findOne({
+		username: body.username,
+	});
 
-    const checkExistingUser = await User.findOne({
-        username: body.username,
-    })
+	if (checkExistingUser) {
+		return res.status(411).json({
+			msg: "username already exists",
+		});
+	}
 
-    if(checkExistingUser){
-        return res.status(411).json({
-            msg: "username already exists",
-        })
-    }
+	const user = await User.create({
+		firstName: body.firstName,
+		lastName: body.lastName,
+		username: body.username,
+		password: body.password,
+	});
 
-    const user = await User.create({
-        firstName: body.firstName,
-        lastName: body.lastName,
-        username: body.username,
-        password: body.password,
-    })
+	const userId = user._id;
 
-    const userId = user._id;
+	const token = jwt.sign(
+		{
+			userId,
+		},
+		JWT_SECRET
+	);
 
-    const token = jwt.sign({
-        userId
-    },JWT_SECRET);
+	res.status(200).json({
+		message: "user created successfully",
+		token: token,
+	});
+});
 
-    res.status(200).json({
-        message:"user created successfully",
-        token: token,
-    })
+router.post("/signin", async (req, res) => {
+	const body = req.body;
+	const {success} = signin.safeParse(body);
+	if (!success) {
+		res.status(411).json({
+			message: "incorrect inputs",
+		});
+	}
+	const user = await User.findOne({
+		username: body.username,
+		password: body.password,
+	});
 
-})
+	if (user) {
+		const token = jwt.sign(
+			{
+				userId: user._id,
+			},
+			JWT_SECRET
+		);
 
-router.post('/signin', async(req,res)=>{
-    const body = req.body;
-    const {success} = signin.safeParse(body);
-    if(!success){
-        res.status(411).json({
-            message:"incorrect inputs",
-        })
-    }
-    const user = await User.findOne({
-        username: body.username,
-        password: body.password,
-    })
+		return res.status(200).json({
+			message: "username and password match found",
+			token: token,
+		});
+	}
 
-    if(user){
-        const token = jwt.sign({
-            userId : user._id,
-        }, JWT_SECRET)
-
-        return res.status(200).json({
-            message:"username and password match found",
-            token: token,
-        })
-    }
-
-    res.status(411).json({
-        message:"Error while logging in",
-    })
-
-})
+	res.status(411).json({
+		message: "Error while logging in",
+	});
+});
 
 module.exports(router);

63212cd7fbed7c5354e3c01b515b6fcfa3dacc5b
aditya <adityadewhy@gmail.com>
2024-05-20 14:38:05 +0530

    bank schema done

--- backend/db.js
+++ backend/db.js
@@ -1,42 +1,55 @@
-
 // backend/db.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/payWallet')
+mongoose.connect("mongodb://localhost:27017/payWallet");
 
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        minLength: 3,
-        maxLength: 30
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 6
-    },
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    }
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		lowercase: true,
+		minLength: 3,
+		maxLength: 30,
+	},
+	password: {
+		type: String,
+		required: true,
+		minLength: 6,
+	},
+	firstName: {
+		type: String,
+		required: true,
+		trim: true,
+		maxLength: 50,
+	},
+	lastName: {
+		type: String,
+		required: true,
+		trim: true,
+		maxLength: 50,
+	},
+});
+
+const userAccountSchema = new mongoose.Schema({
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	balance: {
+		type: Number,
+		required: true,
+		min: 0,
+	},
 });
 
 // Create a model from the schema
-const User = mongoose.model('User', userSchema);
-
+const User = mongoose.model("User", userSchema);
+const userAccount = mongoose.model("userAccount", userAccountSchema);
 module.exports = {
-	User
+	User,
+	userAccount,
 };

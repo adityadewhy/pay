a1b9527a72bce9ced5ddc41e372e6fecfc6230a0
adityadewhy <adityadewhy@gmail.com>
2024-06-03 17:27:16 +0530

    /bulk done except for filtering out logged in user, to start /sendmoney

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,18 +1,25 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import SendMoneyButton from "./SendMoneyButton";
 
 const Users = () => {
 	const [users, setUsers] = useState([]);
+	const [filter, setFilter] = useState("");
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		const fetchUsers = async () => {
 			try {
 				const token = localStorage.getItem("token");
-				const res = await axios.get("http://localhost:3000/api/v1/user/bulk", {
-					headers: {
-						Authorization: `Bearer ${token}`,
-					},
-				});
+				const res = await axios.get(
+					"http://localhost:3000/api/v1/user/bulk?filter=" + filter,
+					{
+						headers: {
+							Authorization: `Bearer ${token}`,
+						},
+					}
+				);
 				setUsers(res.data.users);
 				console.log("users state:", users);
 			} catch (err) {
@@ -20,7 +27,7 @@ const Users = () => {
 			}
 		};
 		fetchUsers();
-	}, []);
+	}, [filter]);
 
 	return (
 		<div>
@@ -28,23 +35,33 @@ const Users = () => {
 				<input
 					type="text"
 					placeholder="Search users.. "
-					className="w-full px-2 py-1 border rounded border-slate-200 mb-2"
+					className="w-full px-2 py-1 border rounded border-slate-200 mb-2 text-black"
+					onChange={(e) => {
+						setFilter(e.target.value);
+					}}
 				/>
 			</div>
 			<div className="userslist">
-                Users List:
-                <ul>
-                    {users.length > 0 ? (
-                        users.map((user) => (
-                            <li key={user.id}>
-                                {user.firstName} {user.lastName} ({user.username})
-                            </li>
-                        ))
-                    ) : (
-                        <p>No users found</p>
-                    )}
-                </ul>
-            </div>
+				Users List:
+				<ul>
+					{users.length > 0 ? (
+						users.map((user) => (
+							<li key={user.id} className="usersList text-1xl font-bold flex flex-row justify-between items-center">
+								<div>
+									{user.firstName} {user.lastName} ({user.username})
+								</div>
+								<SendMoneyButton
+									onClick={(e) => {
+										navigate("/sendmoney");
+									}}
+								/>
+							</li>
+						))
+					) : (
+						<p>No users found</p>
+					)}
+				</ul>
+			</div>
 		</div>
 	);
 };
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
 import Users from "../components/Users";
+import Button from "../components/Button";
 
 function Dashboard() {
 	const [userBalance, setUserBalance] = useState("");
@@ -28,6 +29,10 @@ function Dashboard() {
 		fetchBalance();
 	}, []);
 
+	const signout = () => {
+		console.log("this should ideally log out the user by deleting token");
+	};
+
 	if (wait) {
 		return <div> Fetching balance... </div>;
 	}
@@ -36,14 +41,12 @@ function Dashboard() {
 		<div>
 			<div className="bg-black h-screen text-white flex flex-row justify-center">
 				<div className="Card bg-gray-600 flex flex-col justify-start p-10 pt-6 m-10 rounded gap-3 w-full">
-					<div className="banner flex justify-between mb-0 mt-0 ml-5 mr-5 h-14">
+					<div className="banner flex justify-between mb-0 mt-0 ml-5 mr-5 h-14 items-center">
 						<div className="left flex flex-row justify-between h-full">
 							<img src="logo.png" alt="logo" className="h-full mr-3" />
 							<p className="text-6xl font-bold  h-full">PayWallet</p>
 						</div>
-						<div className="right text-4xl h-full flex flex-row items-center underline">
-							Hello UserName
-						</div>
+						<Button label="Signout" onClick={signout} />
 					</div>
 					<hr className="h-px my-8 mt-1 bg-gray-200 border-1 dark:bg-gray-700" />
 

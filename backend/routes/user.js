883ce643b3b86c35f48c3fd57349368ce7ca5743
adityadewhy <adityadewhy@gmail.com>
2024-06-03 07:56:38 +0530

    still fixing

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -120,7 +120,7 @@ router.put("/", authMiddleware, async (req, res) => {
 	});
 });
 
-router.get("/bulk", async (req, res) => {
+router.get("/bulk", authMiddleware, async (req, res) => {
 	try {
 		const filter = req.query.filter || "";
 		const users = await User.find({
--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -6,22 +6,32 @@ const Users = ()=>{
     const [users, setUsers] = useState([])
 
     useEffect(()=>{
+        console.log("inside useEffect")
         const fetchUsers = async () =>{
+            console.log("inside fetchUsers")
             try{
+                console.log("inside try")
                 const token = localStorage.getItem("token");
+                console.log("token: ",token)
                 const res = await axios.get("http://localhost:3000/api/user/bulk"+filter,{
                     headers:{
                         Authorization:`Bearer ${token}`
                     }
                 })
-                setUsers(res.)
+                console.log("res is: ",res)
+                setUsers(res)
             }catch{
+                console.log("inside catch :(")
                 setUsers("error fetching users");
             }
         }
+        console.log("calling fetchUsers next")
         fetchUsers();
 
-    },[filter])
+    },[])
+
+    console.log("users array: ",users)
+  
     //     {
     //     const token = localStorage.getItem("token");
     //     axios.get("http://localhost:3000/api/v1/user/bulk"+filter,
